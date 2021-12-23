a46f2fc775eddbd4b9b174832ca9bcc35887b1ac
Martin Pohlmann <martin@intevation.de>
2021-12-23 15:35:43 +0100

    Added OS4 news

---
+++ src/app/news/OpenSlides4/OpenSlides4.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-OpenSlides4',
+    templateUrl: './OpenSlides4.component.html',
+    styleUrls: ['./OpenSlides4.component.scss']
+})
+export class ArticleOpenSlides4NewComponent {}
