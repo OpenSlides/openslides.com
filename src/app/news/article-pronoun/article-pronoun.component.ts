7cc32eedf7783c62c6593701d079ff12548918c4
Emanuel Schütze <emanuel@intevation.de>
2024-03-12 10:13:16 +0100

    New article pronoun (#162)

---
+++ src/app/news/article-pronoun/article-pronoun.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-article-pronoun',
+    templateUrl: './article-pronoun.component.html',
+    styleUrls: ['./article-pronoun.component.scss']
+})
+export class ArticlePronounComponent {}
